45ccf7eea0b23f952e27cf7c1b0986571024b686
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-10-08 04:53:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,16 @@
 katz_deli= []
-other_deli=["Logan", "Avi", "Spencer"]
-
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(array)
-  if array.length >=1
-    puts "Currently serving #{array.first}."
-    array.shift
-elsif array.empty?
+def now_serving(katz_deli)
+if katz_deli.length >=1
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+elsif katz_deli.empty?
     puts "There is nobody waiting to be served!"
-    else
-      puts "There is nobody waiting to be served!"
   end
 end
 
@@ -22,20 +18,12 @@ def line(katz_deli)
   if katz_deli.empty? == true
     puts "The line is currently empty."
   else
-     num=1
+     num=0
     current = "The line is currently:"
-katz_deli.each do |person|
-  current += " #{num}. #{person}"
+while num < katz_deli.length do
+  current += " #{num+1}. #{katz_deli[num]}"
    num += 1
    end
     puts current
   end
 end
-
-
-puts line(katz_deli)
-puts take_a_number(katz_deli, "Ada")
-puts now_serving(other_deli)
-puts now_serving(katz_deli)
-puts line(other_deli)
-puts take_a_number(other_deli, "Tom")
